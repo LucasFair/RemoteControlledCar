72c69dfe41f08923d1e57291bd56d247c23dae49
LucasFair <141710445+LucasFair@users.noreply.github.com>
2023-09-11 22:23:22 +0200

    Updating files.

--- ElonMusk/Model/CarRC.cs
+++ ElonMusk/Model/CarRC.cs
@@ -1,6 +1,7 @@
 ﻿using ElonMusk.View;
 using System;
 using System.Drawing;
+using System.Threading;
 
 namespace ElonMusk.Model
 {
@@ -47,21 +48,29 @@ namespace ElonMusk.Model
 		public void Activate()
 		{
 			CarBattery battery = new CarBattery();
+			CarDisplay display = new CarDisplay();
 			CarGUI carGUI = new CarGUI();
 
-			if (battery.BatteryLevel != 0)
+			display.DisplayDistance = 100;
+			battery.BatteryLevel = 20;
+
+
+			for (int i = 0; i < display.DisplayDistance; i++)
 			{
-				for (int metersDriven = 0; metersDriven < battery.BatteryLevel; metersDriven++)
+				if (i % battery.BatteryDrain == 0)
 				{
-					byte currBattery = (byte)(battery.BatteryLevel - battery.BatteryDrain);
-					//int currDist = metersDriven;
+					battery.BatteryLevel -= 1;
+					if (battery.BatteryLevel > 0)
+					{
+						Console.WriteLine(battery.BatteryLevel);
+						Thread.Sleep(600);  // Adds a delay by halting the process of the thread for a given time
+					}
 				}
-			}
-			else
-			{
-				carGUI.BatteryEmpty();
+				else
+				{
+					carGUI.BatteryEmpty();
+				}
 			}
 		}
 	}
 }
-
--- ElonMusk/Program.cs
+++ ElonMusk/Program.cs
@@ -1,6 +1,6 @@
 ﻿using ElonMusk.Controller;
 using System;
-using System.Drawing;
+using System.Threading;
 
 namespace ElonMusk
 {
@@ -8,20 +8,6 @@ namespace ElonMusk
 	{
 		static void Main(string[] args)
 		{
-			/*
-			const ushort MAX_BATTERY = 100;
-			ushort battery;
-			byte drainRate = 20; // 1% per meter
-			int distance;
-
-			distance = 1320;
-
-			for (int i = 0; i <= distance; i++)
-			{
-				if ((i % drainRate) == 0)
-				Console.WriteLine(i);
-			}
-			*/
 			CarController carCtrl = new CarController();
 			carCtrl.Start();
 		}
